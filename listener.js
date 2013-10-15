0fc9429cc4a580b5be2e2c0853cbcc77b1d0b944
kevin <kevinsteck@gmail.com>
2013-10-14 22:52:20 -0400

    Added some better state tracking and track details.

--- listener.js
+++ listener.js
@@ -16,10 +16,12 @@ var SpotifyController = {
 
   track_details: function() {
     var track_details = {};
-    track_details.track_name = document.getElementById('track-name').firstChild.innerHTML;
-    track_details.artist_name = document.getElementById('track-artist').firstChild.innerHTML;
-    track_details.album_art = document.getElementsByClassName('sp-image-img')[0].style.backgroundImage;
-    return track_details;
+    if (document.getElementById('track-name')) {
+      track_details.track_name = document.getElementById('track-name').firstChild.innerHTML;
+      track_details.artist_name = document.getElementById('track-artist').firstChild.innerHTML;
+      track_details.album_art = document.getElementsByClassName('sp-image-img')[0].style.backgroundImage;
+      return track_details;
+    }
   }
 };
 
@@ -31,10 +33,19 @@ chrome.runtime.onConnect.addListener(function(port) {
   var paused = function(e) {
     port.postMessage({state: 'paused'});
   };
+  var spotify_track_change = function(e) {
+    port.postMessage(SpotifyController.track_details());
+  }
+  var get_status = function() {
+    var evnt = document.createEvent("Event");
+    evnt.initEvent("get_status", false, false);
+    document.documentElement.dispatchEvent(evnt);
+  }
 
   // player events
   document.documentElement.addEventListener("spotify_playing", playing, false);
   document.documentElement.addEventListener("spotify_paused", paused, false);
+  document.documentElement.addEventListener("spotify_track_change", spotify_track_change, false);
 
   port.onMessage.addListener(function(msg) {
     switch (msg.action) {
@@ -49,37 +60,36 @@ chrome.runtime.onConnect.addListener(function(port) {
         break;
       case 'status':
         // get initial status
-        var evnt = document.createEvent("Event");
-        evnt.initEvent("get_status", false, false);
-        document.documentElement.dispatchEvent(evnt);
+        get_status();
         break;
       case 'track_details':
         port.postMessage(SpotifyController.track_details());
         break;
     }
-    // if (msg.action != 'track_details') port.postMessage({change: true});
   });
 
   port.onDisconnect.addListener(function() {
       document.documentElement.removeEventListener("spotify_playing", playing, false);
       document.documentElement.removeEventListener("spotify_paused", paused, false);
+      document.documentElement.removeEventListener("spotify_track_change", spotify_track_change, false);
       port = null;
   });
 });
 
 var script_function = function attach_to_spotify() {
+  var send_event = function(event_name) {
+    var evnt = document.createEvent("Event");
+    evnt.initEvent(event_name, false, false);
+    document.documentElement.dispatchEvent(evnt);
+  };
   // for initial status
   document.documentElement.addEventListener("get_status", function() {
     var player = this.SpotifyApi.api._modules['$api/scripts/models'];
     if (player) {
       if (player.player.playing) {
-        var evnt = document.createEvent("Event");
-        evnt.initEvent("spotify_playing", false, false);
-        document.documentElement.dispatchEvent(evnt);
+        send_event("spotify_playing");
       } else {
-        var evnt = document.createEvent("Event");
-        evnt.initEvent("spotify_paused", false, false);
-        document.documentElement.dispatchEvent(evnt);
+        send_event("spotify_paused");
       }
     }
   }.bind(this), false);
@@ -91,22 +101,19 @@ var script_function = function attach_to_spotify() {
       setTimeout(function() {
         if (this.SpotifyApi) {
           if (this.SpotifyApi.api._modules['$api/scripts/models']) {
-            // attach to pause
-            var paused = this.SpotifyApi.api._modules['$api/scripts/models'].player.pause;
-            this.SpotifyApi.api._modules['$api/scripts/models'].player.pause = function() {
-              var evnt = document.createEvent("Event");
-              evnt.initEvent("spotify_paused", false, false);
-              document.documentElement.dispatchEvent(evnt);
-              paused.bind(this)();
-            }
-
-            // attach to play
-            var play = this.SpotifyApi.api._modules['$api/scripts/models'].player.play;
-            this.SpotifyApi.api._modules['$api/scripts/models'].player.play = function() {
-              var evnt = document.createEvent("Event");
-              evnt.initEvent("spotify_playing", false, false);
-              document.documentElement.dispatchEvent(evnt);
-              play.bind(this)();
+            // attach to dispatch
+            var dispatch = this.SpotifyApi.api._modules['$api/scripts/models'].player.dispatchEvent;
+            this.SpotifyApi.api._modules['$api/scripts/models'].player.dispatchEvent = function(e) {
+              if (e.type && e.type == 'change:playing') {
+                if (!e.oldValue) {
+                  send_event("spotify_playing");
+                } else {
+                  send_event("spotify_paused");
+                }
+              } else if (e.type && e.type == 'change:duration') {
+                send_event("spotify_track_change");
+              }
+              dispatch.bind(this)(e);
             }
           }
         }
--- popup.js
+++ popup.js
@@ -14,6 +14,11 @@ function previous() {
 
 function status() {
   sendMessage('status');
+  get_track_details();
+}
+
+function get_track_details() {
+  sendMessage('track_details');
 }
 
 function setup() {
@@ -23,7 +28,7 @@ function setup() {
       if (response.change) {
         // setTimeout(function() { port.postMessage({action: 'track_details'}); }, 1000);
       } else if (response.album_art && response.track_name && response.artist_name) {
-        document.getElementById('album_art').innerHTML = response.album_art;
+        document.getElementById('album_art').style.backgroundImage = response.album_art;
         document.getElementById('track_name').innerHTML = response.track_name;
         document.getElementById('artist_name').innerHTML = response.artist_name;
       } else if (typeof response.state !== "undefined") {
